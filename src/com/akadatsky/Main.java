e81f251de5eeab3b018783ae339570fb00863fea
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2021-11-30 18:39:35 +0200

    Task 1

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -7,21 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Fruit redApple = new Fruit();
-        redApple.setType(Fruit.Type.APPLE);
-        redApple.setColor(Color.RED);
-        redApple.setWeight(232);
-        redApple.setPricePerKg(11.5);
-        System.out.println(redApple);
-
+        Fruit redApple = new Fruit(Fruit.Type.APPLE, Color.RED, 11.5, 232);
         Fruit greenApple = new Fruit(Fruit.Type.APPLE, Color.GREEN, 22.90, 230);
+        Fruit banana = new Fruit(Fruit.Type.BANANA, Color.YELLOW, 28.80, 230);
+        Fruit lemon = new Fruit(Fruit.Type.LEMON, Color.YELLOW, 30.90, 228);
 
         List<Fruit> fruits = new ArrayList<>();
         fruits.add(redApple);
         fruits.add(greenApple);
-
-        // lemon  228g, 30.90
-        // banana  230g, 28.80
+        fruits.add(banana);
+        fruits.add(lemon);
 
         double sum = 0;
 
@@ -29,7 +24,6 @@ public class Main {
             Fruit fruit = fruits.get(i);
             sum += fruit.getPricePerKg() * fruit.getWeight() / 1000.0;
         }
-        System.out.println(sum);
-
+        System.out.printf("Общая сумма чека: %.2f грн", sum);
     }
 }
